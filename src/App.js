b6bb2770bc3570315fbbd830665e81c3485c2c9b
kodingankeren <kodingancerdas@gmail.com>
2021-08-23 14:13:55 +0000

    Jajal

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Cucibos
         </p>
         <a
           className="App-link"
