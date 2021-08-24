79e644957009fc11b7f436819eea8f050edcc0e9
kodingankeren <55055303+kodingankeren@users.noreply.github.com>
2021-08-24 17:27:27 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Cucibos
+          Cucibos <FontAwesomeIcon icon={['fas', 'highlighter']} /> <FontAwesomeIcon icon={['fas', 'code']} />
         </p>
         <a
           className="App-link"
